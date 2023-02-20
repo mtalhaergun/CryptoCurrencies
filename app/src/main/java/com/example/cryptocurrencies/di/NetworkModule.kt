51204fbca4f992e8df19ea3eed5c952addfb4c12
Muhammet Talha Ergün <mtalhaergun24@gmail.com>
2023-02-20 22:56:35 +0300

    Created Hilt and Module

---
+++ app/src/main/java/com/example/cryptocurrencies/di/NetworkModule.kt
@@ -0,0 +1,62 @@
+package com.example.cryptocurrencies.di
+
+import androidx.viewbinding.BuildConfig
+import com.example.cryptocurrencies.network.ApiFactory
+import com.example.cryptocurrencies.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggerInterceptor() : HttpLoggingInterceptor{ // Uygulama debug veya release ise
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG){
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        }else{
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+        return httpLoggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor):OkHttpClient{
+        return OkHttpClient.Builder().readTimeout(60,TimeUnit.SECONDS)
+            .connectTimeout(60,TimeUnit.SECONDS).addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory() : GsonConverterFactory{
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ) : Retrofit{
+        return Retrofit.Builder().baseUrl(BASE_URL).client(okHttpClient).addConverterFactory(gsonConverterFactory).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiFactory(retrofit: Retrofit): ApiFactory{
+        return retrofit.create(ApiFactory::class.java)
+    }
+}
---
+++ app/src/main/java/com/example/cryptocurrencies/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.cryptocurrencies.utils
+
+object Constants {
+    const val BASE_URL = "https://pro-api.coinmarketcap.com/"
+    const val API_KEY = "cba90077-9773-43ac-8f78-7aa955b2ebf4"
+    const val LIMIT = "10"
+}
